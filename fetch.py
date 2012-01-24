7b55bfc713eb1bf0504a8e5d3fb86ee531024234
Nodir Turakulov <turakulov@gmail.com>
2012-01-25 00:27:26 +0500

    fetch got more stable.
    If "tf get" didn't  fetch anything new, then the previous version is fetched and then "tf get" is repeated.

--- fetch.py
+++ fetch.py
@@ -8,6 +8,7 @@ def fetch(cfg):
     lastChangeset = re.findall(r'^\d+', git('notes show $lastCommit'), re.M)[-1]
     print('Last synchronized changeset:', lastChangeset)
   except:
+    lastChangeset = None
     fail('Last changeset could not determined. Probably the last commit is missing a tf note. Commit: %s' % lastCommit)
 
   latestCommit = tf.history('-stopAfter:1')[0].id
@@ -24,6 +25,12 @@ def fetch(cfg):
 
   print('%d changeset(s) to fetch' % len(history))
 
+  def gitHasChanges():
+    return git('status -s')
+
+  def fetch(version):
+    return tf('get -version:%s -recursive .' % version, output = True, dryRun = cfg.dryRun)
+
   if cfg.debug:
     print('Making files read-only')
   chmod('.', False)
@@ -32,14 +39,31 @@ def fetch(cfg):
     for i, cs in enumerate(history):
       printLine()
       print('Fetching [%d/%d] "%s"...' % (i + 1, len(history), cs.line))
+      fetch(cs.id)
+      if not gitHasChanges():
+        print('Nothing new is fetched. TFS thinks that it is up to date.')
+        if i:
+          fail('Please, try it again with --debug option and report the output to the developer.')
+        print('Trying to fetch the previous changeset and repeat.')
+        fetch(lastChangeset)
 
-      tf('get -version:%s -recursive .' % cs.id, output = True, dryRun = cfg.dryRun)
+        def didNotHelp():
+          fail('It did not help. The problem is that TFS thinks that it is up to date,' +
+            'but it is not according to Git. Try to fetch an older version from TFS and fetch/pull again.' +
+            ('Alternatively try to fetch version %s completely:' % lastChangeset) +
+             'tf get -version:%s -force -overwrite -all' % lastChangeset
+          )
 
+        if gitHasChanges(): didNotHelp()
+        print('Now fetching %s again...' % cs.id)
+        fetch(cs.id)
+        if not gitHasChanges(): didNotHelp()
+
+      print('Committing to Git...')
       comment = cs.comment
       if not comment:
         print('The comment is empty. Using changeset number as a comment')
         comment = str(cs.id)
-      print('Committing to Git...')
       git('add -A .', dryRun = cfg.dryRun)
       git(r'commit -m "%s" --author="%s <%s@%s>" --date="%s"' % (comment, cs.committer, cs.committer, cfg.domain, cs.dateIso), output = True, dryRun = cfg.dryRun)
       hash = git('log -1 --format=%H', dryRun = cfg.dryRun and 'abcdef')
