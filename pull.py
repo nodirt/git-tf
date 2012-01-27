eefbd6dbb313c3e16690459ec962d757a4a5b543
Nodir Turakulov <turakulov@gmail.com>
2012-01-27 12:22:11 +0500

    pull prints a descriptive error message with instructions on rebasing error

--- pull.py
+++ pull.py
@@ -5,4 +5,9 @@ def pull(cfg):
   if fetch(cfg) or git('log master..tfs'):
     print('\nRebasing')
     git('checkout master', output = True)
-    git('rebase tfs', output = True)
+    try:
+      git('rebase tfs', output = True)
+    except:
+      print('There were errors while rebasing TFS changes on the master.')
+      print('Please resolve the conflicts.')
+      raise
