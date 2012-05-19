9442db773e2fb1b748a77eb06b7026ad94496c2b
Nodir Turakulov <turakulov@gmail.com>
2012-05-19 22:05:11 +0500

    Implemented `status` command

---
+++ status.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+from core import *
+
+
+class status(Command):
+    """Display commits to be pushed to TFS."""
+
+    def __enter__(self):
+        self.moveToRootDir()
+        self.switchBranch('master')
+
+    def _run(self):
+        commits = git('log tfs.. --format="%h %s"').splitlines()
+        if not commits:
+            print('There are no commits to be pushed to TFS')
+            return
+
+        print('%d commit%s to be pushed to TFS:' % (len(commits), 's' if len(commits) > 1 else ''))
+        for c in commits:
+            printIndented(c)
+
+
+if __name__ == '__main__':
+    status().run()
